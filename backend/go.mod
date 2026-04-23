5e10140dcb275bfa77e4dc46d834c709a2a33ec0
akisatoon <akisatoon@gmail.com>
2026-04-24 01:37:27 +0900

    add: User Create Service in user pacakge.
    Add service.go, repo.go in user package.
    Update draft.sql to add password salt.
    Update go.mod, go.sum to import "golang.org/x/crypto/argon2"

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,10 @@
 module backend
 
-go 1.24.3
+go 1.25.0
 
-require github.com/lib/pq v1.12.3
+require (
+	github.com/lib/pq v1.12.3
+	golang.org/x/crypto v0.50.0
+)
+
+require golang.org/x/sys v0.43.0 // indirect
---
+++ backend/internal/user/service.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"time"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type User struct {
+	ID           int64
+	Username     string
+	PasswordHash string
+	PasswordSalt string
+	CreatedAt    time.Time
+}
+
+type Service struct {
+	repo UserRepository
+}
+
+func (s *Service) Create(username string, password string) error {
+	hash, salt, err := hashPassword(password)
+	if err != nil {
+		return err
+	}
+	usr := &User{
+		Username:     username,
+		PasswordHash: hash,
+		PasswordSalt: salt,
+	}
+	return s.repo.Save(usr)
+}
+
+func hashPassword(password string) (passwdHash string, passwdSalt string, errr error) {
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", "", err
+	}
+
+	// ハッシュアルゴリズムの強度を設定するためのパラメータ
+	iterationTime := uint32(2)
+	memory := uint32(19 * 1024) // KiB単位なので19Mibになる
+	threads := uint8(4)
+
+	keyLen := uint32(32)
+
+	hash := argon2.IDKey([]byte(password), salt, iterationTime, memory, threads, keyLen)
+	return base64.StdEncoding.EncodeToString(hash), base64.StdEncoding.EncodeToString(salt), nil
+}
