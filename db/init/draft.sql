da19fa8b4c4c3785622f6af57f86a365945c9760
akisatoon <akisatoon@gmail.com>
2026-04-25 02:46:11 +0900

    update: delete password_salt in db/init/draft.sql

--- db/init/draft.sql
+++ db/init/draft.sql
@@ -3,7 +3,6 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    password_salt VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
